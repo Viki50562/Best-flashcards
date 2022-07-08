6553852bf4fdcddf8486d47cd93b9724d281b4d1
Ekaterina Ekimova <psich.kawai@gmail.com>
2022-07-08 11:07:05 +0300

    написала метод, который делает для каждой темы массив объектов с вопросами и ответами

--- Model.js
+++ Model.js
@@ -12,8 +12,26 @@ raccooonQuestions;
   
 
   loadQuestionsMenuFromFiles() {
-    // Придумать формат хранение менюшки вопросов и способ нормально разлочить вопрос-ответ файл
+    // функция для перевода из массива строк (полученного из изначального файла) в массив объектов с ключами question и answer
+    function infoToQandA(arr) {
+      const questionAndAnswer = [];
+      for (let i = 0; i < arr.length; i += 3) {
+        const obj = {};
+        obj.question = arr[i];
+        obj.answer = arr[i + 1];
+        questionAndAnswer.push(obj);
+      }
+      return questionAndAnswer
+    }
+
     this.nighthawkQuestions = fs.readFileSync('topics/nighthawk_flashcard_data.txt', 'utf-8').split('\n');
+    this.nighthawkQuestions = infoToQandA(this.nighthawkQuestions);
+
+    this.otterQuestions = fs.readFileSync('topics/otter_flashcard_data.txt', 'utf-8').split('\n');
+    this.otterQuestions =  infoToQandA(this.otterQuestions);
+
+    this.raccooonQuestions = fs.readFileSync('topics/raccoon_flashcard_data.txt', 'utf-8').split('\n');
+    this.raccooonQuestions = infoToQandA(this.raccooonQuestions);
   }
 
   getQuestionListByCategory(category) {
